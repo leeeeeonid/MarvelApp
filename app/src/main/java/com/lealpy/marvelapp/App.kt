67d17cce2af332d46395907cced8bc4ca578130c
lealpy <lealp@mail.ru>
2022-03-10 20:55:44 +0300

    added architecture

---
+++ app/src/main/java/com/lealpy/marvelapp/App.kt
@@ -0,0 +1,7 @@
+package com.lealpy.marvelapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
--- app/src/main/java/com/lealpy/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/com/lealpy/marvelapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.lealpy.marvelapp
+package com.lealpy.marvelapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lealpy.marvelapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
